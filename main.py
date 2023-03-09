51c2fcba98448d0e77872fa5dc2848065575fc9a
AlfredoRodriguez <126662592+Rodriguez-Alfredo@users.noreply.github.com>
2023-03-09 23:22:20 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+#!/bin/python3
+
+#print string
+print ("Hello World!")
+
+#triple quote ("""_""") for multi-lines
+print ("""This string runs
+multiple lines!""")
+
+#concatenate strings with (+)
+print ("This string is " + "awesome!")
+
+#make new line
+print('\n')
+print ('Test that new line out.')
+
+#Thank you TCM Security
